e250e184ce01dceffe054c65d9290f03660dbef6
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-04-15 11:58:21 -0300

    validation update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true, uniqueness: true, length: {maximum:25}
+  validates :title, presence: true, length: {maximum:25}
   validates :body, presence: true
 end
