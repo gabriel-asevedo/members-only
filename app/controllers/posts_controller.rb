1e4a5ebaa5b212fd1d4725db3e044f6c38f93565
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-04-15 15:00:16 -0300

    'create' and 'posts_params' methods created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,23 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!
 
   def new
     @post = Post.new
   end
 
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "Post created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def posts_params
+    params.require(:post).permit(:title, :body)
+  end
 end
+git
