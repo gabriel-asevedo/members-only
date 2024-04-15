309e2c4d61f88110102246b8f014d7ee8d177059
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-04-15 12:37:19 -0300

    Added 'user model' validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }, format: {
+    with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/,
+    message: "must be at least 6 characters long, contain at least one uppercase letter, one lowercase letter, one number and one symbol"
+  }
+
   validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 25 }
 
 end
